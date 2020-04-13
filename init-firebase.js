153e2ba2b040083866b2dab63205cdce0da4fe93
Sofienne LASSOUED <soflass1293@gmail.com>
2020-04-13 11:58:09 +0100

    Add: Added files

---
+++ init-firebase.js
@@ -0,0 +1,9 @@
+// TODO: Replace the following with your app's Firebase project configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBnQHgo7gOQ-ADM6gzALFS3nzWIJJ15gAo",
+  authDomain: "france-covid-19.firebaseio.com/",
+  databaseURL: "https://france-covid-19.firebaseio.com/",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
